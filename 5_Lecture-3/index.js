a74dc706454bf483d3ad5a24e4f081ae1376d78d
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-07 16:40:02 +0500

    added new practice

---
+++ 5_Lecture-3/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Interation with user
+
+// alert("Hello user!");
+
+// let userAnswer = confirm("Тебе есть 18", "");
+// console.log(userAnswer);
+
+// let userCity = prompt("С какого ты города?", "Алматы");
+// console.log(typeof userCity);
+
+//
+let a = 15;
+console.log(a, typeof a);
+console.log(typeof String(a));
+console.log("1" + 5);
+
+let value = true;
+value = String(value);
+console.log(value, typeof value);
+console.log(typeof Number(value), Number(value));
+
+let string = "Hello world";
+console.log(Number(string), typeof Number(string));
+console.log("5" / "2");
+console.log("5" - "2");
+console.log(typeof "5" % "2"); //NaN
+console.log(typeof Number("5" + 1));
