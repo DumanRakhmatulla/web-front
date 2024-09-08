e4f126b724c9b45568b7ab92492e11ecbc4c711b
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-08 23:29:54 +0500

    added practice seminar

---
+++ 6_Seminar_3/index.js
@@ -0,0 +1,22 @@
+// function greeting() {
+//   console.log("Hello World");
+// }
+
+// greeting();
+
+// function greeting(Number) {
+//   console.log(Number);
+// }
+
+// greeting(15);
+
+let accounts = [];
+// person = {
+//   accountNumber Number
+//   holderName ,
+//   balance,
+// };
+
+function createAccount(accountNumber, holderName, balance) {}
+
+createAccount(15, "Raymbek", 500);
