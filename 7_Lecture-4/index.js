3423ab36521485b0d8a6b0ce434c19ad67e7a706
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-10 01:22:14 +0500

    added lecture 7

--- 7_Lecture-4/index.js
+++ 7_Lecture-4/index.js
@@ -9,17 +9,44 @@
 // const html = `<div>Hello, ${user}</div>`;
 // document.write(html);
 
-const fruites = {
-  apples: 2,
-  stroubery: 1,
-};
+// const fruites = {
+//   apples: 2,
+//   stroubery: 1,
+// };
 
-const veg = {
-  carrots: 3,
-};
+// const veg = {
+//   carrots: 3,
+// };
 
-const { apples } = fruites;
-const { carrots } = veg;
+// const { apples } = fruites;
+// const { carrots } = veg;
 
-const FruintAndVeg = { ...fruites, ...veg };
-console.log(FruintAndVeg);
+// const FruintAndVeg = { ...fruites, ...veg };
+// console.log(FruintAndVeg);
+
+///--------------
+// Function
+
+// ------------
+// Array methods
+//-------------
+
+// const array = [1, 2, 3, 4, 5, 6];
+// console.log(array.length);
+// array[array.length] = 5;
+
+// array.push(100); // довавляеть последный элемент
+// console.log(array);
+// array.pop(); // удаляет последный элемент
+// console.log(array);
+// array.shift(); // удаляет первый элемент
+// console.log(array);
+// array.unshift(100); // довавляеть первый элемент
+
+// let arr = [];
+// const s = "Aruzhan Samat Duman Ospan";
+// arr = s.split(" ");
+// console.log(arr);
+
+let array1 = [1, 12, 26, 25, 7, 6];
+console.log(array1.sort); // 1 12 25 26 6 7
