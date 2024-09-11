829e1c903db97607c489eaa2b96bc8d71947f398
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-12 00:22:07 +0500

    updated

--- 7_Lecture-4/homework/index.js
+++ 7_Lecture-4/homework/index.js
@@ -11,16 +11,38 @@
 
 // let f = pow(4, 2);
 // console.log(pow(3, 3));
-let arr = [2, 1, 3, 4, -5];
-// alert(arr);
+// let arr = [2, 1, 3, 4, -5];
+// // alert(arr);
 
-let vasya = { name: "Вася", age: 25 };
-let petya = { name: "Петя", age: 30 };
-let masha = { name: "Маша", age: 28 };
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [vasya, petya, masha];
+// // console.log(users);
+// // console.log(users.name);
+
+// let names = users.map((user) => user.name);
+// alert(names);
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
 
 let users = [vasya, petya, masha];
-// console.log(users);
-// console.log(users.name);
 
-let names = users.map((user) => user.name);
-alert(names);
+let usersMapped = users.map((user) => ({
+  fullName: `${user.name} ${user.surname}`,
+  id: user.id,
+}));
+/* ... ваш код ... */
+// usersMapped = [
+//   { fullName: "Вася Пупкин", id: 1 },
+//   { fullName: "Петя Иванов", id: 2 },
+//   { fullName: "Маша Петрова", id: 3 },
+// ];
+
+// console.log(usersMapped.fullName);
+
+alert(usersMapped[0].id); // 1
+alert(usersMapped[0].fullName); // Вася Пупкин
