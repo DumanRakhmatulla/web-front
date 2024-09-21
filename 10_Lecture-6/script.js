5b729ae3378d6d014879c62b42065268b88d05ad
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-21 19:02:15 +0500

    added lecture 10

---
+++ 10_Lecture-6/script.js
@@ -0,0 +1,11 @@
+// DOM element onclick
+
+const btn = document.querySelector(".btn");
+
+// btn.onclick = function () {
+//   alert("Hello");
+// };
+
+btn.addEventListener("click", () => {
+  alert("hello");
+});
