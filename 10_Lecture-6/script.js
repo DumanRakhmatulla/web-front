f39d250f4007fec4bad5d8ebfc02c31d30b666b5
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-21 20:09:06 +0500

    updated

--- 10_Lecture-6/script.js
+++ 10_Lecture-6/script.js
@@ -1,11 +1,44 @@
 // DOM element onclick
 
-const btn = document.querySelector(".btn");
+const btn = document.querySelectorAll(".btn")[0],
+  btn1 = document.querySelectorAll(".btn")[2],
+  btn2 = document.querySelectorAll(".btn")[1],
+  body = document.querySelector("body");
 
 // btn.onclick = function () {
 //   alert("Hello");
 // };
 
-btn.addEventListener("click", () => {
-  alert("hello");
+btn.addEventListener("click", (e) => {
+  //   alert("hello");
+  e.target.remove();
+});
+
+let i = 0;
+
+const count = function () {
+  console.log(i, "i increasing");
+  i++;
+  if (i === 3) {
+    btn1.removeEventListener("click", count);
+  }
+};
+
+btn1.addEventListener("click", count);
+
+btn2.addEventListener("mouseover", function (e) {
+  console.log(e);
+  btn2.style.backgroundColor = "black";
+});
+
+btn2.addEventListener("mouseout", function (e) {
+  console.log(e);
+  btn2.style.backgroundColor = "blue";
+});
+
+body.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Enter") {
+    alert("you pressed D");
+  }
 });
