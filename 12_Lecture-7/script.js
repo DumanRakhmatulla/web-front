919845a53d1029b648326896c7e4081196a8fb9b
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-27 18:05:29 +0500

    added lecture 7

---
+++ 12_Lecture-7/script.js
@@ -0,0 +1,11 @@
+const sayHello = () => {
+  console.log("Hello");
+};
+
+// setTimeout(sayHello, 5000);
+
+// setTimeout(function () {
+//   console.log("Timeout1");
+
+//   setTimeout(console.log("Timeout2"), 500);
+// }, 1000);
