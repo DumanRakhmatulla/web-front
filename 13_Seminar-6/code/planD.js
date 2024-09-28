ed520aafc7cffbb8f644a67efb67f2a47e682f32
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-29 03:58:49 +0500

    updated

--- 13_Seminar-6/code/planD.js
+++ 13_Seminar-6/code/planD.js
@@ -7,3 +7,27 @@
 // периодичностью в 1 секунду
 // `stopTextColor` должна реагировать на нажатие кнопки `#start`
 // также, должна очистить интервал и переписать глобальную переменную `window.plans.planD=true;`
+
+let flashLightsInterval = null;
+
+function changeColor() {
+  if (flashLightsInterval === null) {
+    flashLightsInterval = setInterval(flashText, 1000);
+  }
+}
+
+function flashText() {
+  const police = document.getElementById("police");
+  police.className = police.className === "go" ? "stop" : "go";
+}
+
+function stopTextColor() {
+  clearInterval(flashLightsInterval);
+  flashLightsInterval = null;
+}
+
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+
+startButton.addEventListener("click", changeColor);
+stopButton.addEventListener("click", stopTextColor);
