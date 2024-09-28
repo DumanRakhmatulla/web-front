80b4ac3323152c7e464464e908768c6480a66dc1
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-28 18:37:03 +0500

    updated

--- 13_Seminar-6/code/planA.js
+++ 13_Seminar-6/code/planA.js
@@ -1,3 +1,19 @@
 // Создать функцию 'createPlan' которая после 5 секунд будет возвращать промис с двумя методами `.then`
 // Первый метод должен вернуть в консоль текст: "План придуман, полетели!"
 // Второй метод должен переписать глобальную переменную `window.plans.planA=true;`
+
+function createPlan(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("План придуман, полетели!");
+    }, time);
+  });
+}
+
+createPlan(5000)
+  .then((res) => {
+    console.log("План придуман, полетели!");
+  })
+  .then(() => {
+    window.plans.planA = true;
+  });
