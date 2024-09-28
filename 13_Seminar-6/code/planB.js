8c3e4fa57ad864e484a537566f5ab7229611a28f
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-29 03:21:09 +0500

    updated

--- 13_Seminar-6/code/planB.js
+++ 13_Seminar-6/code/planB.js
@@ -1,3 +1,17 @@
 // Создать функцию `placeABomb` которая должна взорваться через 2 секунды с текстом "BANG!"
 // Взрыв должен быть в `reject`
 // Метод `catch` должен переписать глобальную переменную `window.plans.planB=true;`
+
+const placeABomb = (time) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      rej("BANG!");
+    }, time);
+  });
+};
+
+placeABomb(2000).catch((e) => {
+  console.error(e);
+  console.log(e);
+  window.plans.planB = true;
+});
--- 13_Seminar-6/code/planC.js
+++ 13_Seminar-6/code/planC.js
@@ -3,3 +3,20 @@
 // и после 4 секунд должна вернуть в resolve "close the road in 4 seconds"
 // Если два условия выполнены вывести в консоль "we almost did it!" и
 // переписать глобальную переменную `window.plans.planC=true;`
+
+function leaveFast(message, time) {
+  return new Promise((res, rej) => {
+    setTimeout(() => res(message), time);
+  });
+}
+
+Promise.all([
+  leaveFast("leave the bank in 2 seconds", 2000),
+  leaveFast("close the road in 4 seconds", 4000),
+])
+  .then((res) => {
+    console.log("we almost did it!", res);
+  })
+  .then(() => {
+    window.plans.planC = true;
+  });
