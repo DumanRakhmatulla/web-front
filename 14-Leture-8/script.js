98047a0da25743e0fd9a10f200afc8a97791d77f
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-09-30 00:33:21 +0500

    updated

--- 14-Leture-8/script.js
+++ 14-Leture-8/script.js
@@ -9,14 +9,46 @@ const user = {
   number: "+77763537764",
 };
 
-console.log(user);
-console.log(JSON.stringify(user));
+// console.log(user);
+// console.log(JSON.stringify(user));
 
 const response = JSON.stringify(user);
-console.log(JSON.parse(response));
+// console.log(JSON.parse(response));
 
 // Копирование обектов
 const person1 = JSON.parse(response);
 person1.name = "Samat";
-console.log(user);
-console.log(person1);
+// console.log(user);
+// console.log(person1);
+
+//XMLHttpRequest
+const inputUSD = document.querySelector("#usd"),
+  inputKZT = document.querySelector("#kzt");
+
+// inputKZT.addEventListener("input", () => {
+//   //   console.log("KZT works!");
+//   const request = new XMLHttpRequest();
+//   request.open("GET", "./data.json");
+//   request.setRequestHeader("Content-type", "app/json");
+//   request.send();
+
+//   request.addEventListener("readystatechange", () => {
+//     if (request.readyState === 4 && request.status === 200) {
+//       console.log(request.response);
+//       const data = JSON.parse(request.response);
+//       console.log(data);
+//       inputUSD.value = +inputKZT.value * data.current.usd;
+//     } else {
+//       inputUSD.value = "Error";
+//     }
+//   });
+// });
+
+inputKZT.addEventListener("input", async () => {
+  let response = await fetch("./data.json");
+
+  if (response.ok) {
+    const data = await response.json();
+    inputUSD.value = +inputKZT.value / data.current.usd;
+  }
+});
