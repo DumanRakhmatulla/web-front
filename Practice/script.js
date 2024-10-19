c15641451c4034eef8a03401a778560e6f93510b
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-10-19 15:58:09 +0500

    I did an practice async

--- Practice/script.js
+++ Practice/script.js
@@ -21,22 +21,38 @@
 // then() - обрабатывает fulfilled состояние
 // catch() - обрабтывает rejected состояние
 
-const promise = new Promise((fulfill, reject) => {
-  console.log("Начало, состояние pending...");
+// const promise = new Promise((fulfill, reject) => {
+//   console.log("Начало, состояние pending...");
 
-  setTimeout(() => {
-    if (Math.random() > 0.5) {
-      fulfill("Ура, состояние fulfilled ");
-    } else {
-      reject("Увы, состояние rejected ");
-    }
-  }, 1000);
-});
+//   setTimeout(() => {
+//     if (Math.random() > 0.5) {
+//       fulfill("Ура, состояние fulfilled ");
+//     } else {
+//       reject("Увы, состояние rejected ");
+//     }
+//   }, 1000);
+// });
 
-promise
-  .then((successDat) => {
-    console.log("Успех! получено данные: ", successDat);
-  })
-  .catch((errorData) => {
-    console.log("Ошибка. Получены данные: ", errorData);
-  });
+// promise
+//   .then((successDat) => {
+//     console.log("Успех! получено данные: ", successDat);
+//   })
+//   .catch((errorData) => {
+//     console.log("Ошибка. Получены данные: ", errorData);
+//   });
+
+async function getSomething() {
+  return new Promise((fulfill) => {
+    setTimeout(() => {
+      fulfill("Привет!");
+    }, 3000);
+  }); // Ключевое слово async перед функцией означает, что эта функция всегда будет возвращать Promise
+}
+
+const something = await getSomething();
+
+console.log(something);
+
+// getSomething().then((something) => {
+//   console.log(something);
+// });
