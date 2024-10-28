063e23a6cc1e52ccac9b99ceabb29fec2ef69b32
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-10-28 11:49:16 +0500

    upadated

--- 19_Seminar-9/my_code/my-seminar-app/src/App.js
+++ 19_Seminar-9/my_code/my-seminar-app/src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+// jsx
+import BgImg from "./images/pattern-background-desktop.svg";
+import IllHero from "./images/illustration-hero.svg";
+import IconMusic from "./images/icon-music.svg";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        height: "100vh",
+        background: `url(${BgImg})`,
+        backgroundRepeat: "no-repeat",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          borderRadius: "20px",
+          backgroundColor: "white",
+          display: "flex",
+          flexDirection: "column",
+          gap: "40px",
+        }}
+      >
+        <div>
+          <img
+            src={IllHero}
+            style={{
+              width: "400px",
+              borderRadius: "20px",
+            }}
+          ></img>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            gap: "20px",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h1
+            style={{
+              margin: "0",
+            }}
+          >
+            Order Summary
+          </h1>
+          <p
+            style={{
+              margin: "0",
+            }}
+          >
+            You can now listen to millions of songs, <br /> audiobooks, and
+            podcasts on any device <br /> anywhere you like!
+          </p>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-around",
+              backgroundColor: "#D6E1FF",
+              borderRadius: "20px",
+              alignItems: "center",
+              width: "350px",
+              height: "80px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "15px",
+              }}
+            >
+              <img
+                src={IconMusic}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                }}
+              ></img>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+
+                  gap: "10px",
+                }}
+              >
+                <p
+                  style={{
+                    margin: "0",
+                  }}
+                >
+                  Annual Plan
+                </p>
+                <p
+                  style={{
+                    margin: "0",
+                  }}
+                >
+                  $59.99/year
+                </p>
+              </div>
+            </div>
+            <a>Change</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
