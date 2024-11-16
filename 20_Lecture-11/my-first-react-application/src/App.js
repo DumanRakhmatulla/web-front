0fd98af933ed0fa0120a5cf21bc6252b4e86ad94
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-11-16 22:23:02 +0500

    updated

--- 20_Lecture-11/my-first-react-application/src/App.js
+++ 20_Lecture-11/my-first-react-application/src/App.js
@@ -2,51 +2,80 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 
-const FieldfuncstionComponenet = () => {
-  const Admin = false;
-  const holder = "Enter here";
-  const styleFieldAdmin = { width: "300px", backgroundColor: "red" };
-  const styleFieldUser = { width: "200px", backgroundColor: "green" };
+// const FieldfuncstionComponenet = () => {
+//   const Admin = false;
+//   const holder = "Enter here";
+//   const styleFieldAdmin = { width: "300px", backgroundColor: "red" };
+//   const styleFieldUser = { width: "200px", backgroundColor: "green" };
+//   return (
+//     <input
+//       placeholder={holder}
+//       type="text"
+//       style={Admin ? styleFieldAdmin : styleFieldUser}
+//     />
+//   );
+// };
+
+// class FieldClassComponent extends React.Component {
+//   constructor() {
+//     super();
+//     this.name = "Field1";
+//     this.surname = "Field1";
+//     this.age = "Field1";
+//   }
+
+//   clickMe() {
+//     const { name, surname, age } = this;
+//     console.log("You clicked me", name);
+//   }
+
+//   render() {
+//     const holder = "Enter here";
+//     return <input placeholder={holder} type="text" />;
+//   }
+// }
+
+function Person(props) {
+  console.log(props);
   return (
-    <input
-      placeholder={holder}
-      type="text"
-      style={Admin ? styleFieldAdmin : styleFieldUser}
-    />
+    <div>
+      <h1>My name is {props.name}</h1>
+      <a href={props.link} />
+    </div>
   );
-};
-
-class FieldClassComponent extends React.Component {
-  constructor() {
-    super();
-    this.name = "Field1";
-    this.surname = "Field1";
-    this.age = "Field1";
-  }
-
-  clickMe() {
-    const { name, surname, age } = this;
-    console.log("You clicked me", name);
-  }
-
-  render() {
-    const holder = "Enter here";
-    return <input placeholder={holder} type="text" />;
-  }
 }
 
 function App() {
   const logged = false;
   const ButtonLoggedText = "nFactorial";
-  const ButtonNotLoggedText = "You not logged";
+
+  const users = [
+    {
+      id: 1,
+      name: "Duman",
+      link: "inst.com",
+    },
+    {
+      id: 2,
+      name: "Dauren",
+      link: "inst.com",
+    },
+    {
+      id: 3,
+      name: "Ernur",
+      link: "facebook.com",
+    },
+  ];
 
   return (
     <div className="App">
-      {}
       <Header />
-      <p>{logged ? ButtonLoggedText : ButtonNotLoggedText}</p>
-      <FieldfuncstionComponenet />
-      <FieldClassComponent />
+      {users.map((item, index) => (
+        <Person key={index} name={item.name} link={item.link} />
+      ))}
+
+      {/* <FieldfuncstionComponenet />
+      <FieldClassComponent /> */}
     </div>
   );
 }
