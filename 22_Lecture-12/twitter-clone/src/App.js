4c45600a14eab637560f0bfa14de47959eca6f8a
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-11-18 11:27:41 +0500

    i added new project name is twitter-clone

---
+++ 22_Lecture-12/twitter-clone/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Home from "./components/HomeComponent";
+import SideMenu from "./components/SideMenuComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <SideMenu />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ 22_Lecture-12/twitter-clone/src/components/SideMenuComponent/index.js
@@ -0,0 +1,5 @@
+function SideMenu() {
+  return <p>SideMenu</p>;
+}
+
+export default SideMenu;
