8e265be9b193fa6b4110dddd291f067c6c49a346
Duman <154539968+DumanRakhmatulla@users.noreply.github.com>
2024-11-18 16:01:09 +0500

    updated

--- 23_Seminar-11/first-vite-project/src/App.jsx
+++ 23_Seminar-11/first-vite-project/src/App.jsx
@@ -1,5 +1,12 @@
+import Button from "react-bootstrap/Button";
+
 function App() {
-  return <>Hello</>;
+  return (
+    <div>
+      <button>Click me</button>
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
